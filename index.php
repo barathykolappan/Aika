530bea059b1a213f08bc84837bf348dfce3737e6
barathykolappan <barathykolappan@gmail.com>
2018-06-12 19:50:01 +0530

    Iteration 1.3
    Aika 1.0

--- index.php
+++ index.php
@@ -1,41 +1,10 @@
 <?php 
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
-if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
-	$json = json_decode($requestBody);
-
-	$text = $json->result->parameters->text;
-
-	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
-
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
-		
-		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
-			break;
-	}
-
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response);
-}
-else
-{
-	echo "Method not allowed";
-}
-
+    
+	$html=file_get_contents("https://www.rottentomatoes.com/m/godfather/");
+    $split=explode("<span class=\"meter-vale superPageFontColor\"><span>",explode("</span>",$html)[0])[1];
+	echo $split;
 ?>
+
+
+
+
