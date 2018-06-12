13154a33420848ee69ce6ca0074ebe1073f52c1c
barathykolappan <barathykolappan@gmail.com>
2018-06-12 19:55:08 +0530

    Iteration 1.4
    Aika 1.0

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php 
-    
-	$html=file_get_contents("https://www.rottentomatoes.com/m/godfather/");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+    $html=file_get_contents("https://www.rottentomatoes.com/m/godfather/");
     $split=explode("<span class=\"meter-vale superPageFontColor\"><span>",explode("</span>",$html)[0])[1];
-	echo $split;
+	echo json_encode($split);
+}
+else
+{
+	echo "Method not allowed";
+}
+
 ?>
 
 
