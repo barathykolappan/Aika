bc92bb81ac3fa743bc36f1ee01c54fb8ca6d0e11
barathykolappan <barathykolappan@gmail.com>
2018-06-12 21:05:54 +0530

    Iteration 1.8
    Aika 1.0

--- index.php
+++ index.php
@@ -7,20 +7,20 @@ if($method == 'POST'){
 	$text = $json->result->parameters->text;
 
 	$array = array(
-    "Shawshank redemption" =>90,
-	"Godfather" =>80,
-	"The Dark Knight" =>70);
+    "Shawshank redemption" =>'90',
+	"Godfather" =>'80',
+	"The Dark Knight" =>'70');
     $split=$array[$text];
-	switch ($text) {
-		case 90:
+	switch ($split) {
+		case '90':
 			$speech = "Awesome work by the crew.";
 			break;
 
-		case 80:
+		case '80':
 			$speech = "Worth your tickets.";
 			break;
 
-		case 70:
+		case '70':
 			$speech = "Decent movie.";
 			break;
 		
