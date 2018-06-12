8726a98f5d8a9b4964ea9e6c2770d5eb60c800e9
barathykolappan <barathykolappan@gmail.com>
2018-06-12 20:06:59 +0530

    Iteration 1.5 (Test)
    Aika 1.0

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php 
 
-$method = $_SERVER['REQUEST_METHOD'];
+//$method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
-if($method == 'POST'){
+//if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
     $html=file_get_contents("https://www.rottentomatoes.com/m/godfather/");
     $speech=explode("<span>",explode("</span>",$html)[0])[1];
-    $response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response);
-}
-else
-{
-	echo "Method not allowed";
-}
+ //   $response = new \stdClass();
+	//$response->speech = $speech;
+	//$response->displayText = $speech;
+	//$response->source = "webhook";
+	echo $speech;
+//	echo json_encode($response);
+//}
+//else
+//{
+//	echo "Method not allowed";
+//}
 
 ?>
 
