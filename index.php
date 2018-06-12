2fe4dd0f8335e6fb9ee738209b16967b87ed004a
barathykolappan <barathykolappan@gmail.com>
2018-06-12 20:53:20 +0530

    Iteration 1.6 (Test)
    Aika 1.0

--- index.php
+++ index.php
@@ -36,32 +36,9 @@ if($method == 'POST'){
 	"Prestige" => int(85)
 );
     $split=int($array[$text]);
-	if($split<10)
-		$Rev="This defines Garbage";
-	else if($split>=10 && $split<20)
-		$Rev="Couldn't be Worse, a Rubbish piece.";
-	else if($split>=20 && $split<30)
-		$Rev="Series of mistakes, unattended.";
-	else if($split>=30 && $split<40)
-		$Rev="Not worth your tickets.";
-	else if($split>=40 && $split<50)
-		$Rev="This is a Cat on the Wall, you decide!";
-	else if($split>=50 && $split<60)
-		$Rev="A Fair piece, which could've been better.";
-	else if($split>=60 && $split<70)
-		$Rev="A Decent movie, that could be worth your time.";
-	else if($split>=70 && $split<80)
-		$Rev="Bang on! This is going to be worth your tickets.";
-	else if($split>=80 && $split<90)
-		$Rev="Goodness. An Awesome work by the crew. Dont miss it.";
-	else if($split>=90 && $split<100)
-		$Rev="Wow. An absolute Masterpiece!";
-	else if($split==100)
-		$Rev="You dare not miss this movie.";
-
 	$response = new \stdClass();
-	$response->speech = $Rev;
-	$response->displayText = $Rev;
+	$response->speech = $split;
+	$response->displayText = $split;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
