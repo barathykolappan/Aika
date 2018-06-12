59977d6c3edf01426e9d8aa74e666792c687d9bc
barathykolappan <barathykolappan@gmail.com>
2018-06-12 19:59:18 +0530

    Iteration 1.4
    Aika 1.0

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
     $html=file_get_contents("https://www.rottentomatoes.com/m/godfather/");
-    $split=explode("<span class=\"meter-vale superPageFontColor\"><span>",explode("</span>",$html)[0])[1];
-	echo json_encode($split);
+    $speech=explode("<span class=\"meter-vale superPageFontColor\"><span>",explode("</span>",$html)[0])[1];
+    $response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
 }
 else
 {
