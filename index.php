7e040f0141d8100637c602f303c37e701dad0fe4
barathykolappan <barathykolappan@gmail.com>
2018-06-12 20:58:20 +0530

    Iteration 1.6 (Reverse)
    Aika 1.0

--- index.php
+++ index.php
@@ -9,36 +9,27 @@ if($method == 'POST'){
 
 	$text = $json->result->parameters->text;
 
-	$array = array(
-    "Shawshank redemption" => int(92),
-    "The Shawshank redemption" => int(92),
-	"Godfather" => int(92),
-	"The Godfather" => int(92),
-	"The Dark Knight" => int(90),
-	"Dark Knight" => int(90),
-	"12 Angry Men" => int(89),
-	"Twelve Angry Men" => int(89),
-	"Schindler's List " => int(89),
-	"Pulp Fiction" => int(89),
-	"The Good, the Bad and the Ugly" => int(88),
-	"Fight Club" => int(88),
-	"Forrest Gump" => int(87),
-	"Inception" => int(87),
-	"Goodfellas" => int(87),
-	"Seven" => int(86),
-	"The Silence of the Lambs" => int(86),
-	"Silence of the Lambs" => int(86),
-	"The Usual Suspects" => int(86),
-	"Usual Suspects" => int(86),
-	"Interstellar" => int(85),
-	"Psycho" => int(85),
-	"The Prestige" => int(85),
-	"Prestige" => int(85)
-);
-    $split=$array[$text];
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
 	$response = new \stdClass();
-	$response->speech = $split;
-	$response->displayText = $split;
+	$response->speech = $speech;
+	$response->displayText = $speech;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
