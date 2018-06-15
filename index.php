cfa61bbc6e346e84c5a65a6f0a8cf1a7031d0fe1
barathykolappan <barathykolappan@gmail.com>
2018-06-15 10:22:24 +0530

    Iteration 4.1
    Precision of query return with movie concat.

--- index.php
+++ index.php
@@ -97,7 +97,8 @@ class Imdb
     }
 	if($fl==0)
 	{
-    $url = "http://www.${engine}.com/search?q=IMDb+" . rawurlencode($title);
+    $url0 = "http://www.${engine}.com/search?q=IMDb+" . rawurlencode($title);
+	$url=$url0."+ Movie";
     $ids = $this->match_all('/<a.*?href="https?:\/\/www.imdb.com\/title\/(tt\d+).*?".*?>.*?<\/a>/ms', $this->geturl($url), 1);
     if (!isset($ids[0]) || empty($ids[0])) //if search failed
       return $this->getIMDbIdFromSearch($title, $nextEngine); //move to next search engine
