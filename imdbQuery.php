4a60f4edd3d53c9279a17304d79d83bade7ef214
barathykolappan <barathykolappan@gmail.com>
2018-06-16 23:41:35 +0530

    Iteration 7.3
    FINAL

--- imdbQuery.php
+++ imdbQuery.php
@@ -45,7 +45,7 @@ class Imdb
 	}
 	else
 	{
-	$ssplit=explode("<section class=\"titlereference-section-overview\">",explode("<\/div>",$html)[0])[1];
+	$ssplit=explode("<section class=\"titlereference-section-overview\">",explode("</div>",$html)[0])[1];
 	$split=explode("<div>",explode(".",$ssplit)[0])[1];
 	}
 	$score=$arr['rating']*10;
